152bebfeeb66ac42bcf2d7586e22e403e0554d7e
unknown <codingdatta@gmail.com>
2024-05-20 17:43:46 +0530

    makes some changes

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const showCase = ref(false);
 
 function newtaskadder() {
     const newTask={
-        id:id.value++,
+        id:id.value,
         taskname:dummy.value,
         taskDes:taskDes.value
     }
@@ -24,18 +24,24 @@ function newtaskadder() {
     
     dummy.value="";
     taskDes.value="";
+    id.value++;
     showCase.value = !showCase.value;
+}
+function taskdelete(id){
+  alert(id);
 }
 </script>
 
 <template>
     <main>
         <h1>todolist</h1>
-        <div v-for="(items,key) in  newtodo" :key="key" class="taskCard">
+        <div class="card-container">
+          <div v-for="(items,key) in  newtodo" :key="key" class="taskCard">
             
             <h3>{{ items.taskname }}</h3>
             <p>{{ items.taskDes }}</p>
-         
+           <button @click="taskdelete(key)">delete</button>
+        </div>
         </div>
         <div class="overlay" v-if="showCase">
         <div class="card">
@@ -45,7 +51,7 @@ function newtaskadder() {
           <button @click="handleButtonClick">Close</button>
         </div>
       </div>
-        <button @click="handleButtonClick" v-if="!showCase">Show</button>
+        <button @click="handleButtonClick" v-if="!showCase" class="taskBtn">Add new task</button>
          
          
     </main>
@@ -56,8 +62,12 @@ function newtaskadder() {
     width: 270px;
     height: 100px;
     border: 0.5px black solid;
-    border-radius: 20px;
+    border-radius: 5px;
+    padding: 10px;
     margin: 5px;
+ }
+ .taskBtn{
+  margin:5px 0px 0px 10px;
  }
  .overlay {
     position: fixed;
@@ -77,4 +87,5 @@ function newtaskadder() {
     background-color: white;
     padding: 20px;
   }
+  
 </style>
